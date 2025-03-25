95fb765157f59932e7b7b3dffd6165ee051f9972
m-nyberg <marienyberg07@gmail.com>
2025-03-25 13:51:06 +0100

    limte inn navbar

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function storBoks() {
     }
 
 }
+
+
+
+function klikk() {
+    const dropdownElm = document.querySelector(".dropdown-content2")
+    if (dropdownElm.style.display == "flex") {
+        dropdownElm.style.display = "none"
+    } else {
+        dropdownElm.style.display = "flex"
+    }
+}
+
